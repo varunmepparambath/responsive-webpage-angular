a1fa26dc0343bb9790edcb9a0ded823e9d608401
VARUN M <varunmepparambath@gmail.com>
2021-03-05 16:51:45 +0530

    video centered with auto play

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -54,8 +54,8 @@ declare var $: any;
       <div class="col"></div>
     </div>
 
-    <div class="home__vimeo" style="margin:0px;padding:0px;overflow:hidden">
-      <div style="padding:56.25% 0 0 0;position:relative;">
+    <div class="home__vimeo">
+      <!-- <div style="padding:56.25% 0 0 0;position:relative;">
         <iframe
           src="https://player.vimeo.com/video/495701517?title=0&byline=0&portrait=0"
           style="position:absolute;top:0;left:0;width:100%;height:80%;overflow:hidden;"
@@ -63,8 +63,17 @@ declare var $: any;
           allow="autoplay; fullscreen; picture-in-picture"
           allowfullscreen
         ></iframe>
-      </div>
-      <script src="https://player.vimeo.com/api/player.js"></script>
+      </div> -->
+      <!-- <script src="https://player.vimeo.com/api/player.js"></script> -->
+      <iframe
+      class="home__vimeo__iframe"
+        src="https://player.vimeo.com/video/495701517?autoplay=1&loop=1&title=0&byline=0&portrait=0"
+        width="1600"
+        height="900"
+        frameborder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   `,
   styles: [],
